dfe1fdb027f2bf81c98d6c37f6f6410a473026a2
Cenk Kılıç <cenk@kilic.dev>
2024-09-14 22:31:49 +0200

    WIP: .

--- lua/youtrack/config.lua
+++ lua/youtrack/config.lua
@@ -84,28 +84,10 @@ function M.read()
 	return M.options
 end
 
----Calculate the size of the UI.
----@param size youtrack.ConfigUiSize
----@return  youtrack.ConfigUiSize
-local function calculate_size(size)
-	if size.width and size.width <= 1 and size.width > 0 then
-		size.width = math.floor(vim.o.columns * size.width)
-	end
-	if size.height and size.height <= 1 and size.height > 0 then
-		size.height = math.floor(vim.o.lines * size.height)
-	end
-
-	return size
-end
-
 ---@param config youtrack.Config
 ---@return youtrack.Config
 function M.setup(config)
 	M.options = vim.tbl_deep_extend("force", {}, defaults, config or {})
-
-	calculate_size(M.options.ui)
-	calculate_size(M.options.issues.size)
-	calculate_size(M.options.issue.size)
 
 	return M.options
 end
